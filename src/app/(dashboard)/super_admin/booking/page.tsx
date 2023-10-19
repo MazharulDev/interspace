91b9183a0b7367361cc925273c36d5137bdb64b9
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-10-20 02:21:44 +0600

    final project

---
+++ src/app/(dashboard)/super_admin/booking/page.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import { useDebounced } from "@/redux/hooks";
+import { Button, Input, message } from "antd";
+import Link from "next/link";
+import { useState } from "react";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  ReloadOutlined,
+  EyeOutlined,
+} from "@ant-design/icons";
+import dayjs from "dayjs";
+import ISTable from "@/components/ui/ISTable";
+import BreadCrumb from "@/components/ui/Breadcrumb";
+import Actionbar from "@/components/ui/ActionBar";
+import { useDeleteUserMutation, useUsersQuery } from "@/redux/api/userApi";
+import { getUserInfo } from "@/services/auth.service";
+import {
+  useBookingByEmailQuery,
+  useBookingsQuery,
+  useDeleteBookingMutation,
+} from "@/redux/api/bookingApi";
+
+const MyBookingConnection = () => {
+  const { role, userId } = getUserInfo() as any;
+  const query: Record<string, any> = {};
+
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  query["limit"] = size;
+  query["page"] = page;
+  query["sortBy"] = sortBy;
+  query["sortOrder"] = sortOrder;
+
+  const debouncedSearchTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedSearchTerm) {
+    query["searchTerm"] = debouncedSearchTerm;
+  }
+  const { data, isLoading } = useBookingByEmailQuery(userId);
+  const [deleteBooking] = useDeleteBookingMutation();
+  const handleDelete = async (id: string) => {
+    const res: any = await deleteBooking(id).unwrap();
+
+    if (res?._id) {
+      message.success("Booked canceled");
+    }
+  };
+
+  const bookings = data;
+
+  const columns = [
+    {
+      title: "Package name",
+      dataIndex: "packageName",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Created at",
+      dataIndex: "createdAt",
+      render: function (data: any) {
+        return data && dayjs(data).format("MMM D, YYYY hh:mm A");
+      },
+      sorter: true,
+    },
+
+    {
+      title: "Action",
+      render: function (data: any) {
+        return (
+          <>
+            {data?.status === "pending" && (
+              <Button
+                onClick={() => handleDelete(data._id)}
+                type="primary"
+                danger
+              >
+                cancel
+              </Button>
+            )}
+            {data?.status === "accepted" && <Button type="primary">Pay</Button>}
+          </>
+        );
+      },
+    },
+  ];
+  const onPaginationChange = (page: number, pageSize: number) => {
+    setPage(page);
+    setSize(pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
+  const resetFilters = () => {
+    setSortBy("");
+    setSortOrder("");
+    setSearchTerm("");
+  };
+  return (
+    <div>
+      <BreadCrumb
+        items={[
+          {
+            label: `${role}`,
+            link: `/${role}`,
+          },
+          {
+            label: "booking",
+            link: `/${role}/booking`,
+          },
+        ]}
+      />
+      <Actionbar title="Booking List">
+        <Input
+          size="large"
+          placeholder="Search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          style={{
+            width: "20%",
+          }}
+        />
+        <div>
+          {(!!sortBy || !!sortOrder || !!searchTerm) && (
+            <Button
+              style={{ margin: "0px 5px" }}
+              type="primary"
+              onClick={resetFilters}
+            >
+              <ReloadOutlined />
+            </Button>
+          )}
+        </div>
+      </Actionbar>
+
+      <ISTable
+        loading={isLoading}
+        columns={columns}
+        dataSource={bookings}
+        pageSize={size}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
+    </div>
+  );
+};
+
+export default MyBookingConnection;
